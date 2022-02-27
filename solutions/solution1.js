51d7071985d267976e0c94433f0e5250dd1af2d8
Garrett Brown <garrettpaulbrown@gmail.com>
2022-02-27 12:02:41 -0800

    Solution 1 added.

--- solutions/solution1.js
+++ solutions/solution1.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    $("button1").submit(function(event) {
-
+    const array = iceCreamFlavors
+    let flavorList = [];
+    array.forEach(function(element) {
+        flavorList.push("<li>" + element + "</li>" + "<br>");
     });
-});
+
+    // let final = flavorList.toString();
+    console.log(flavorList);
+    
+    $(".ice-cream-list").append(flavorList);
+ })
---
+++ solutions/solution3.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+//     const array1 = iceCreamFlavors;
+//     let list = array1.toString()
+
+//     $("#answer").text("Solution 3: " + list);
+//  })
